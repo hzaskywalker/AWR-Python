205822ad96ac9e4a6f65da1a0a56dedae282b4f9
hzaskywalker@gmail.com <hzaskywalker@gmail.com>
2020-06-03 13:37:07 -0700

    awr

--- awr2/awr.py
+++ awr2/awr.py
@@ -1,12 +1,12 @@
-# how to parallel awr?
-# we do rollout in parallel, thus we need also parallelize the polices and synchronize the gradient..
+# parallel awr.. don't know if it will work fine
 
 import torch
+import tqdm
+from torch import nn
 import copy
 import numpy as np
-from .models import actor, critic
-from robot.utils.normalizer import Normalizer
-from robot.utils import soft_update, Timer
+from models import actor, critic
+from normalizer import Normalizer
 from collections import deque
 import multiprocessing
 
@@ -16,8 +16,12 @@ def _get_flat_params_or_grads(network, mode='params'):
     include two kinds: grads and params
     """
     attr = 'data' if mode == 'params' else 'grad'
-    #return np.concatenate([getattr(param, attr).cpu().numpy().flatten() for param in network.parameters()])
-    return torch.cat([getattr(param, attr).reshape(-1).cpu() for param in network.parameters()])
+    out = [] 
+    for param in network.parameters():
+        xx = getattr(param, attr) 
+        if xx is not None: 
+            out.append(xx.reshape(-1).cpu())
+    return torch.cat(out)
 
 
 def _set_flat_params_or_grads(network, flat_params, mode='params'):
@@ -29,29 +33,41 @@ def _set_flat_params_or_grads(network, flat_params, mode='params'):
     pointer = 0
     for param in network.parameters():
         #getattr(param, attr).copy_(torch.tensor(flat_params[pointer:pointer + param.data.numel()]).view_as(param.data))
-        getattr(param, attr).copy_(flat_params[pointer:pointer + param.data.numel()].view_as(param.data))
+        xx = getattr(param, attr)
+        if xx is None: continue
+        xx.copy_(flat_params[pointer:pointer + param.data.numel()].view_as(param.data))
         pointer += param.data.numel()
 
 
 class AsyncDDPGAgent:
-    def __init__(self, observation_space, action_space, temp=1., max_weight=20,
-                 gamma=0.99, actor_lr=5e-5, critic_lr=0.01, tau=0.05, update_target_period=1, device='cpu',
+    def __init__(self, observation_space, action_space,
+                 discount=0.99, td_lambda=0.95,
+                 temp=1., max_weight=20, action_std=0.4,
+                 actor_lr=0.0001, critic_lr=0.01, device='cpu',
                  batch_size=256, pipe=None):
 
         self.device = device
         inp_dim = observation_space.shape[0]
-        self.actor = actor(inp_dim, action_space).to(self.device)
-        self.critic = critic(inp_dim).to(self.device)
-        self.normalizer = Normalizer((inp_dim,), default_clip_range=5).to(self.device)
+        self.actor = actor(inp_dim, action_space.low.shape[0], std=action_std).to(device)
+        self.critic = critic(inp_dim).to(device)
+        self.normalizer = Normalizer((inp_dim,), default_clip_range=5).to(device)
         self.normalizer.count += 1
         self.temp = temp
         self.max_weight = max_weight
 
+        # NOTE: optimizer is different
         self.optim_actor = torch.optim.Adam(self.actor.parameters(), actor_lr)
         self.optim_critic = torch.optim.Adam(self.critic.parameters(), critic_lr)
         self.pipe = pipe
         self.batch_size = batch_size
         self.mse = nn.MSELoss()
+
+        self.discount = discount
+        self.td_lambda = td_lambda
+        self.val_norm = 1.0 / (1.0 - self.discount)
+
+        self.action_mean = ((action_space.high + action_space.low)/2)[None,:]
+        self.action_std  = ((action_space.high - action_space.low)/2)[None,:]
 
     def set_params(self, params):
         assert isinstance(params, dict)
@@ -65,11 +81,11 @@ class AsyncDDPGAgent:
         }
 
 
-    def sync_grads(self, net, weights=1.):
+    def sync_grads(self, net, weight=None):
         # reweight the gradients to avoid any bias...
         grad = _get_flat_params_or_grads(net, mode='grad')
-        if weights != 1.:
-            grad = grad * weights
+        if weight is not None:
+            grad = grad * weight
         self.pipe.send(grad)
         grad = self.pipe.recv()
         _set_flat_params_or_grads(net, grad, mode='grad')
@@ -91,35 +107,44 @@ class AsyncDDPGAgent:
     def as_state(self, s):
         return self.normalizer(self.tensor(s))
 
-    def update_actor(self, steps, states, actions, advs):
+    def gen(self, sample_idx, steps, batch_size):
+        while steps > 0:
+            np.random.shuffle(sample_idx)
+            for i in range(len(sample_idx)//batch_size):
+                if steps <= 0:
+                    break
+                yield sample_idx[i*batch_size:(i+1)*batch_size]
+                steps -= 1
+
+    def update_actor(self, steps, states, normed_actions, normed_advs, sample_idx, process_weight=None):
         # we assume all numpy states is not normalized ...
+        #NOTE: it's better to have uniform sampling
         num_idx = len(states)
-        for i in range(steps):
+        for idx in self.gen(sample_idx, steps, self.batch_size):
             self.optim_actor.zero_grad()
-            idx = np.randon.sample(num_idx, self.batch_size)
-            weights = np.minimum(np.exp(advs[idx]/self.temp), self.max_weight)
+            weights = np.minimum(np.exp(normed_advs[idx]/self.temp), self.max_weight)
             weights = self.tensor(weights)
-            logpi = -self.actor(self.as_state(states[idx])).log_probs(self.tensor(actions[idx]))
-            assert logpi.shape == weights.shape
+            distribution = self.actor(self.as_state(states[idx]))
+            logpi = - distribution.log_prob(self.tensor(normed_actions[idx])).sum(dim=-1)
+            assert logpi.shape == weights.shape, f"logpi size: {logpi.shape}, weights size: {weights.shape}"
             actor_loss = (logpi * weights).mean()
             actor_loss.backward()
 
-            self.sync_grads(self.actor)
+            self.sync_grads(self.actor, process_weight)
             self.optim_actor.step()
 
-    def update_critic(self, steps, states, targets):
+    def update_critic(self, steps, states, targets, sample_idx, process_weight=None):
         num_idx = len(states)
-        for i in range(steps):
+        for idx in self.gen(sample_idx, steps, self.batch_size):
             self.optim_critic.zero_grad()
 
-            idx = np.randon.sample(num_idx, self.batch_size)
-            s = self.as_state(states[idx])
-            v = self.tensor(targets[idx])
-            assert s.shape == v.shape
-            critic_loss = self.mse(s, v)
+            normed_val = self.critic(self.as_state(states[idx]))
+            normed_target = self.tensor(targets[idx]) / self.val_norm
+            assert normed_val.shape == normed_target.shape
+            critic_loss = self.mse(normed_val, normed_target)
             critic_loss.backward()
 
-            self.sync_grads(self.critic)
+            self.sync_grads(self.critic, process_weight)
             self.optim_critic.step()
 
     def tocpu(self, x):
@@ -132,33 +157,53 @@ class AsyncDDPGAgent:
             a = p.sample()
         else:
             a = p.mean()
-        return tocpu(a)
+        # unnoramlize action
+        return self.tocpu(a) * self.action_std + self.action_mean
 
     def value(self, obs):
-        return self.critic(self.as_state(obs))
+        # the value networks' output is unnormalized term ..
+        return self.tocpu(self.critic(self.as_state(obs)) * self.val_norm)
 
-    def update(self, buffer, actor_step, critic_step):
+    def update(self, buffer, critic_steps, actor_steps, ADV_EPS=1e-5, process_weight=None):
         states, actions, rewards, dones = [np.array(i) for i in buffer]
-        value = self.critic(self.as_state(states))
+        # normalize action
+        actions = (actions - self.action_mean)/self.action_std
 
-    def discount_return(self, reward, done, value):
-        value = value.squeeze()
+        dones = dones.astype(np.bool)
+
+        valid_mask = ~dones
+        sample_idx = np.arange(len(states))[valid_mask]
+
+        values = self.value(states)
+        discount_return = self.discount_return(rewards, dones, values, self.discount, self.td_lambda)
+        #print('critic', critic_steps, end='\n\n')
+
+        self.update_critic(critic_steps, states, discount_return, sample_idx, process_weight=process_weight)
+        #print('finish critic', end='\n\n')
+
+        # normalize advantages..
+        adv = discount_return - values
+        adv_valid = adv[valid_mask]
+        adv_norm = (adv - adv_valid.mean())/(adv.std() + ADV_EPS)
+
+        #print('actor', actor_steps)
+        self.update_actor(actor_steps, states, actions, adv_norm, sample_idx, process_weight=process_weight)
+        #print('finish actor')
+
+
+    def discount_return(self, reward, done, value, discount, td_lambda):
         num_step = len(value)
-        discounted_return = np.zeros([num_step])
+        return_t = np.zeros([num_step])
 
-        gae = 0
-        for t in range(num_step - 1, -1, -1):
+        nxt = 0
+        for t in range(num_step-1, -1, -1):
             if done[t]:
-                delta = reward[t] - value[t]
+                nxt = value[t]
             else:
-                delta = reward[t] + gamma * value[t + 1] - value[t]
-            gae = delta + gamma * lam * (1 - done[t]) * gae
-
-            discounted_return[t] = gae + value[t]
-
-        # For Actor
-        adv = discounted_return - value
-        return discounted_return, adv
+                nxt_return = reward[t] + discount * nxt
+                nxt = (1.0 - td_lambda) * value[t] + td_lambda * nxt_return
+                return_t[t] = nxt_return
+        return return_t
 
 
 class Worker(multiprocessing.Process):
@@ -170,44 +215,40 @@ class Worker(multiprocessing.Process):
 
     def __init__(self, make, env_name,
                  replay_buffer_size=50000, sample_size=2048,
-                 noise_eps=0.2, random_eps=0.3,
-                 batch_size=256, future_K=4, seed=0, **kwargs):
+                 seed=0, is_primary=False, **kwargs):
         super(Worker, self).__init__()
         self.make = make
         self.env_name = env_name
 
         self.replay_buffer_size = replay_buffer_size
         self.sample_size = sample_size
-
-        self.random_eps = random_eps
-        self.batch_size = batch_size
         self.seed = seed
-
         self.kwargs = kwargs
 
+        self.is_primary = is_primary
+        self.daemon = True
         self.pipe, self.worker_pipe = multiprocessing.Pipe()
         self.start()
 
     def run(self):
         # initialize
-        self.env = self.make(self.env_name)
-        self.env.seed(self.seed)
-        self.env.reset()
+        env = self.make(self.env_name)
+        env.seed(self.seed)
+        env.reset()
+
         import random
         random.seed(self.seed)
         np.random.seed(self.seed)
         torch.manual_seed(self.seed)
         torch.cuda.manual_seed(self.seed)
-        if '_max_episode_steps' in self.env.__dict__:
-            self.T = self.env._max_episode_steps
 
-        agent = AsyncDDPGAgent(self.env.observation_space, self.env.action_space, **self.kwargs, pipe=self.worker_pipe)
+        agent = AsyncDDPGAgent(env.observation_space, env.action_space, **self.kwargs, pipe=self.worker_pipe)
         agent.seed = self.seed
 
-        states = deque(self.replay_buffer_size)
-        actions = deque(self.replay_buffer_size)
-        rewards = deque(self.replay_buffer_size)
-        dones = deque(self.replay_buffer_size)
+        states = deque(maxlen=self.replay_buffer_size)
+        actions = deque(maxlen=self.replay_buffer_size)
+        rewards = deque(maxlen=self.replay_buffer_size)
+        dones = deque(maxlen=self.replay_buffer_size)
         episode = 0
         optim_iter = 0
 
@@ -216,43 +257,55 @@ class Worker(multiprocessing.Process):
             if op == self.EXIT:
                 break
             elif op == self.START:
-                actor_step, critic_step, new_samples = data
+                new_samples, critic_steps, actor_steps = data
 
                 # new rollout
                 obs = env.reset()
+
+                episodes = []
                 episode_reward = 0
                 num = 0
-
                 # --------------- totally prallel ------------------
                 while True:
-                    action = agent.act(obs)
                     # feed the actions into the environment
-                    states.append(np.array(obs))
-                    obs, r, done, info = self.env.step(obs)
+                    states.append(obs)
+                    action = agent.act(obs[None,:])[0]
+                    obs, r, done, info = env.step(action)
 
                     actions.append(action)
                     rewards.append(r)
                     dones.append(False)
-
                     num += 1
+
                     episode_reward += r
                     if done:
+                        # last transitions..
+                        states.append(np.array(obs))
+                        actions.append(action)
+                        rewards.append(0)
+                        dones.append(True)
+                        num += 1
+
                         episode += 1
                         if num >= new_samples:
                             break
                         obs = env.reset()
+                        episodes.append(episode_reward)
                         episode_reward=0
 
                 optim_iter += 1
-                new_states = states[-num:] 
-                if optim_iter == 1: #TODO: update 1
-                    agent.update_normalizer(new_states)
-                
-                agent.update([states, actions, rewards, dones], actor_step, critic_step)
+                states_ = np.array(states)
+                new_states = states_[-num:] 
 
-                if optim_iter != 1: # TODO: update 2
-                    agent.update_normalizer(new_states)
-                self.worker_pipe.send(None)
+                agent.update_normalizer(new_states)
+
+                if self.is_primary:
+                    print(f'\n {optim_iter}: episode num: {len(episodes)}, reward: {np.mean(episodes)}, average length: {num/len(episodes)}')
+
+                # process weight is 1, ideally it shouldn't, but we don't care about it now ...
+                agent.update([states_, actions, rewards, dones], critic_steps, actor_steps, process_weight=1)
+
+                self.worker_pipe.send("FINISH ONE ITER")
 
             elif op == self.ASK:
                 action = agent.act(data)
@@ -278,39 +331,40 @@ class Worker(multiprocessing.Process):
         return self.pipe.recv()
 
 
-class DDPGAgent:
-    def __init__(self, n, num_epoch, n_rollout, timestep, n_batch=50, *args, recorder=None, seed=123, **kwargs):
+class AWR:
+    def __init__(self, n, num_iter=10000,
+            new_samples=2048,
+            critic_update_steps=200,
+            actor_update_steps=1000, *args,
+            seed=123, **kwargs):
+
         self.workers = []
         for i in range(n):
-            self.workers.append(Worker(*args, timestep=timestep, **kwargs, seed=seed + i))
-        self.recorder = recorder
-        self.start(num_epoch, n_rollout, timestep, n_batch)
+            self.workers.append(Worker(*args, **kwargs, seed=seed + i, is_primary=(i==0)))
+        self.start(num_iter, new_samples, critic_update_steps, actor_update_steps)
 
-    def start(self, num_epoch, n_rollout, timestep, n_batch):
+    def start(self, num_iter, new_samples, critic_update_steps, actor_update_steps):
         primary = self.workers[0]
         params = primary.get_params()
         for i in self.workers:
             i.set_params(params)
 
-        start_command = [primary.START, [n_rollout, timestep, n_batch]]
-        for i in self.workers:
-            i.send(start_command)
-        for epoch_id in range(num_epoch):
+        start_command = [primary.START, [new_samples, critic_update_steps, actor_update_steps]]
+        for iter_id in tqdm.trange(num_iter):
+            for i in self.workers:
+                i.send(start_command)
+
             self.update_normalizer()
 
-            for i in range(n_batch):
+            for i in tqdm.trange(critic_update_steps):
                 self.reduce(mode='sum') # for critic
+
+            for i in tqdm.trange(actor_update_steps):
                 self.reduce(mode='sum') # for actor
 
-            for i in self.workers[1:]:
-                i.recv(); i.send(start_command)
+            for i in self.workers:
+                out = i.recv()
 
-            # primary is special
-            if self.recorder is not None:
-                train_info = primary.recv()
-                print(f"EPOCH {epoch_id}: REWARD {train_info[0]}")
-                self.recorder.step(self, *train_info)
-            primary.send(start_command)
 
     def __call__(self, observation):
         self.workers[0].send([self.workers[0].ASK, observation])
@@ -326,14 +380,13 @@ class DDPGAgent:
             i.send(grad)
 
     def update_normalizer(self):
-        s, sq, count, n = 0, 0, 0, len(self.workers)
+        s, sq, count = 0, 0, 0
         for i in self.workers:
-            _s, _sq, _count = i.recv()
+            data = i.recv()
+            _s, _sq, _count = data
             s += _s; sq += _sq; count += _count
-        s /= n; sq /= n; count /= n
         for i in self.workers:
             i.send([s, sq, count])
-
 
     def __del__(self):
         for i in self.workers:
--- awr2/models.py
+++ awr2/models.py
@@ -10,12 +10,9 @@ the input x in both networks should be [o, g], where o is the observation and g 
 
 # define the actor network
 class actor(nn.Module):
-    def __init__(self, inp_dim, oup_dim, action_space, std=0.2):
+    def __init__(self, inp_dim, oup_dim, std=0.2):
         super(actor, self).__init__()
-        self.min = nn.Parameter(torch.tensor(action_space.low), requires_grad=False)
-        self.max = nn.Parameter(torch.tensor(action_space.high), requires_grad=False)
-        self.logstd = nn.Parameter(torch.ones(self.min.shape) * np.log(std), requires_grad=False)
-
+        self.logstd = nn.Parameter(torch.ones(oup_dim) * np.log(std), requires_grad=False)
         self.fc1 = nn.Linear(inp_dim, 128)
         self.fc2 = nn.Linear(128, 64)
 
@@ -28,9 +25,7 @@ class actor(nn.Module):
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        #actions = self.max_action * torch.tanh(self.action_out(x))
-        #return actions
-        action_mean = self.action_out(x) * (self.max - self.min)[None, :] + self.min[None, :]
+        action_mean = self.action_out(x)
         return Normal(action_mean, self.logstd.exp()[None,:].expand_as(action_mean))
 
 class critic(nn.Module):
@@ -43,6 +38,5 @@ class critic(nn.Module):
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
         q_value = self.q_out(x)
-        return q_value
+        return q_value[:, 0]
